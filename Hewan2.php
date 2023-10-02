6ce1c6643484949fed13a0cc9a1b1a3c63f090d3
arip86 <akhmadarip86@gmail.com>
2023-10-02 11:01:42 +0700

    pertemuan 6

---
+++ Hewan2.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'Abstract1.php';
+class Kambing extends Hewan{
+    public function bersuara(){
+        echo "<br> Suara Kambing";
+    }
+}
+
+
+?>
---
+++ objectBidang.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'Lingkaran.php';
+
+$l1 = new Lingkaran(5);
+
+
+$l1 ->keliling();
+// $l1 ->luasBidang();
+// $l1 = $luas->luasBidang();
+//selesaikan agar hasil lingkaran dapat dilihat beserta jari-jari
+
+
+
+?>
